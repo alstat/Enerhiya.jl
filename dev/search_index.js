3e517061b5fc3c9203e2991503f4cf2d809171ec
Documenter.jl <documenter@juliadocs.github.io>
2022-06-10 12:51:50 +0000

    build based on 433d4b9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Enerhiya.jl","page":"Home","title":"Enerhiya.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Enerhiya.jl","category":"page"}]
+}
